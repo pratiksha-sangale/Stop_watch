8b6eaaf5724d9e5374c433153ba2a162a730b3be
Pratiksha Sangale <83330262+pratiksha-sangale@users.noreply.github.com>
2022-10-29 16:48:09 +0530

    Task5

---
+++ README.md
@@ -0,0 +1,6 @@
+# Stop-watch-app
+About Oasis Infobyte Android development 
+
+Task5
+
+This task aims for a simple stop watch app.
---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.stopwatch;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Chronometer chronometer;
+    private long pauseOffset;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chronometer);
+        chronometer.setFormat("Time: %s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                if ((SystemClock.elapsedRealtime() - chronometer.getBase()) >= 10000000) {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    Toast.makeText(MainActivity.this, "Bing!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void startChronometer(View v) {
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+    }
+
+    public void holdChronometer(View v) {
+        if (running) {
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+
+    public void stopChronometer(View v) {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        chronometer.stop();
+        running = false;
+        pauseOffset = 0;
+
+    }
+}
